5ce0ab5f3f5a56b79576be471826d915ee0bc626
Aditya Jayaraman <adit99@example.com>
2015-02-07 13:47:11 -0800

    added tab bars

---
+++ RottenTomatoes/RottenTomatoes/MovieTabViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTabViewController.swift
+//  RottenTomatoes
+//
+//  Created by Aditya Jayaraman on 2/7/15.
+//  Copyright (c) 2015 Aditya Jayaraman. All rights reserved.
+//
+
+import UIKit
+
+
+class MovieTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
